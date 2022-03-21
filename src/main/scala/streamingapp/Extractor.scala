7dc6e5e7e5cbab3b61051149e24f4eac05fd4320
Manasi Belekar <manasi.belekar@zendesk.com>
2022-03-21 13:04:19 +1100

    Fix logic

--- src/main/scala/streamingapp/Extractor.scala
+++ src/main/scala/streamingapp/Extractor.scala
@@ -9,23 +9,23 @@ import io.circe.parser._
 
 object Extractor {
   def extractPages(filename: String): Observable[Page] = {
-    val x: Task[Page] = readFile(filename).map(stringToPage).flatMap(Task.fromEither(_))
-    val y: Task[Observable[Page]] = x.map(generateStream(_, Observable.empty))
-    Observable.fromTask(y).flatten
+    val x: Observable[Page] = Observable.fromTask(readFile(filename).map(stringToPage).flatMap(Task.fromEither(_)))
+    x.flatMap(generateStream(_, Observable.empty))
   }
 
   private def generateStream(page: Page, pages: Observable[Page]): Observable[Page] = {
     page.next match {
       case Some(value) =>
-       val readNext: Task[Page] = readFile(value).map(stringToPage).flatMap(Task.fromEither(_))
+        val readNext: Task[Page] = readFile(value).map(stringToPage).flatMap(Task.fromEither(_))
         val nextPage: Observable[Page] = Observable.fromTask(readNext)
-        pages ++ nextPage
-      case None => Observable.empty[Page]
+        nextPage.flatMap(p => generateStream(p, pages.append(page)))
+      case None => pages.append(page)
     }
   }
 
   // Wrapped in a task to make it re-usable
   private def readFile(filename: String): Task[String] = Task {
+    println(s"Reading file ${filename}")
     val json = Source.fromFile(filename)
     json.getLines().mkString("")
   }
--- src/main/scala/streamingapp/Main.scala
+++ src/main/scala/streamingapp/Main.scala
@@ -1,13 +1,13 @@
 package streamingapp
 
 import io.circe.parser._
-
-import scala.io.Source
+import monix.execution.Scheduler.Implicits.global
+import monix.reactive.Observable
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val json = Source.fromFile("/Users/manasi.belekar/project/practice/streaming-app/data/01.json")
-    val jsonString = json.getLines().mkString("")
-    println(decode[Page](jsonString))
+    val stream: Observable[Page] = Extractor.extractPages("/Users/manasi.belekar/project/practice/streaming-app/data/01.json").head
+    val pageList = stream.toListL.runSyncUnsafe()
+    println(pageList)
   }
 }
